e444baf0d0ba3b1bf5160657cac1d276ad143953
Aviv Rozenboim <aviv@soluto.com>
2019-06-16 19:01:54 +0300

    Configurable management port

--- src/management/index.js
+++ src/management/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const process = require('process');
 const app = express();
 
 let stats = '';
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send(stats);
 });
 
-app.listen(80, () => console.log('Started listening on port 80'));
+app.listen(process.env.PORT || 80, () => console.log('Started listening on port 80'));
 
 module.exports = {
     updateStats(newStats) {
