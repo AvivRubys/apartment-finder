0c6fab3a5219b1fd6a32f9ceed3a2af37c5f0dab
Aviv Rozenboim <aviv@soluto.com>
2019-06-16 18:32:42 +0300

    Default logger to just log the ad url

--- src/dispatcher/index.js
+++ src/dispatcher/index.js
@@ -1,16 +1,16 @@
-const dispatchConfig = require("../nconf").get("dispatcher");
+const dispatchConfig = require('../nconf').get('dispatcher');
 
 switch (dispatchConfig.type) {
-  case "email":
-    module.exports = require("./email");
-    break;
-  case "github":
-    module.exports = require("./github");
-    break;
-  case "slack":
-    module.exports = require("./slack");
-    break;
-  default:
-    console.warn("No/bad dispatcher configuration - defaulting to console.log");
-    module.exports = console.log;
+    case 'email':
+        module.exports = require('./email');
+        break;
+    case 'github':
+        module.exports = require('./github');
+        break;
+    case 'slack':
+        module.exports = require('./slack');
+        break;
+    default:
+        console.warn('No/bad dispatcher configuration - defaulting to console.log');
+        module.exports = ad => console.log(ad.url);
 }
